0ad31b7f8d44592c6625fd6907fe953478881dae
Nacho García <lupus.zgz@gmail.com>
2017-09-20 11:08:32 +0100

    Add Expenses component

--- frontend/src/components/Expenses.vue
+++ frontend/src/components/Expenses.vue
@@ -1,27 +1,69 @@
 <template>
-  <div>
-    {{ expenses[0].id }}
-  </div>
+  <b-container>
+    <b-row>
+      <b-col>
+        <b-form-fieldset horizontal label="Filter" :label-cols="1">
+          <b-form-input v-model="filter" placeholder="Type to Search" />
+        </b-form-fieldset>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-table
+        striped
+        hover
+        responsive
+        bordered
+        :items="expenses"
+        :fields="fields"
+        :filter="filter"
+        :sort-by="date"
+        :sort-desc=true
+        @row-clicked="selectExpense(item)"
+      >
+        <template slot="status" scope="data">{{data.item.state}} {{data.item.modified_by_reviewer}}</template>
+      </b-table>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-import * as Vuex from 'vuex';
-export default {
-  name: 'Expenses',
-  computed: {
-    ...Vuex.mapGetters(['expenses']),
-  },
-  methods: {
-    ...Vuex.mapActions(['updateExpenses']),
-
-    selectExpense (expense) {
-      this.$store.commit('expense', expense);
-      //this.$router.push('site');
+  import * as Vuex from 'vuex';
+  export default {
+    name: 'Expenses',
+    data: () => ({
+      filter: null,
+      fields: {
+        id: {
+          label: 'ID',
+          sortable: false,
+        },
+        date: {
+          label: 'Date',
+          sortable: true,
+        },
+        concept: {
+          label: 'Concept',
+          sortable: false,
+        },
+        status: {
+          label: 'Status',
+        },
+      },
+    }),
+    computed: {
+      ...Vuex.mapGetters(['expenses']),
     },
-  },
+    methods: {
+      ...Vuex.mapActions(['updateExpenses']),
 
-  created () {
-    this.$store.dispatch('updateExpenses')
+      selectExpense (expense) {
+        this.$store.commit('expense', expense);
+        //this.$router.push('site');
+      },
+    },
+
+    created () {
+      this.updateExpenses()
+    }
   }
-}
 </script>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -20,7 +20,14 @@ const mutations = {
 const actions = {
   updateExpenses ({ commit, state }) {
     // here will be the API call
-    commit('expenses', [ {id: 'an id'} ])
+    const expenses = [
+      { id: '456a7a8a9876', userId: '1423oi213j1', activityId: '31i3h1oi3', date: new Date(2017,4,2), concept: 'Qcon London', state:'sumbitted', modified_by_reviewer: true },
+      { id: '4564314d9876', userId: '31k2k3j1kj3', activityId: '3ij21i3i3', date: new Date(2016,5,1), concept: 'Client expenses', state:'sumbitted', modified_by_reviewer: false },
+      { id: '567b90s9765v', userId: '32kj3123jk2', activityId: '54j343kl5', date: new Date(2017,8,3), concept: 'Barcelona Trip', state:'reviewed', modified_by_reviewer: false },
+      { id: '567890s9876s', userId: '4t12f41t2f4', activityId: '5i43j534j', date: new Date(2015,3,2), concept: 'Recruiting expenses', state:'saved', modified_by_reviewer: false },
+      { id: 'asd23456f765', userId: 'usadhoi12j3', activityId: '5o454j455', date: new Date(2017,4,2), concept: 'Catch-Up', state:'payed', modified_by_reviewer: true },
+    ]
+    commit('expenses', expenses)
   }
 }
 
