8bad4da6c46e2e9dc3dd787fd8d45e076f811b87
Nacho García <lupus.zgz@gmail.com>
2017-09-21 14:00:03 +0100

    Add amount to the expense submission

--- frontend/src/components/ExpenseSubmission.vue
+++ frontend/src/components/ExpenseSubmission.vue
@@ -2,7 +2,7 @@
   <b-container>
     <b-form @submit="submitExpense">
       <b-form-row>
-        <b-col>
+        <b-col cols-md="5">
           <h2>
             <b-form-group
               id="expenseSubmissionConceptGroup"
@@ -18,7 +18,7 @@
           </h2>
         </b-col>
 
-        <b-col>
+        <b-col cols-md="5">
           <h2>
             <b-form-group
               id="expenseSumbissionDateGroup"
@@ -34,16 +34,57 @@
           </h2>
         </b-col>
 
-        <b-col>
+        <b-col cols-md="2">
           <h2>
-          <b-badge :variant="getStateVariant(expenseSubmission.state)">{{expenseSubmission.state}}</b-badge>
+            <b-form-group
+              id="expenseSumbissionStateGroup"
+              label="State" label-for="expenseSubmissionState"
+            >
+              <b-badge
+                pill
+                id="expenseSubmissionState"
+                :variant="getStateVariant(expenseSubmission.state)"
+                class="text-capitalize"
+              >
+                {{expenseSubmission.state}}
+              </b-badge>
+            </b-form-group>
           </h2>
         </b-col>
       </b-form-row>
 
       <h2>Expenses</h2>
       <b-card-group columns>
-        <b-card v-for="expense in expenseSubmission.expenses">
+        <b-card v-for="expense in expenseSubmission.expenses" :key="expense.date+expense.concept">
+
+          <b-form-row>
+            <b-col cols="10">
+              <b-form-group
+                horizontal
+                id="expenseDateGroup"
+                label="Date" label-for="expenseDate"
+              >
+                <b-form-input
+                  required
+                  id="expenseDate"
+                  v-model="expense.date"
+                  type="date"
+                />
+              </b-form-group>
+            </b-col>
+
+            <b-col cols="2">
+              <b-button
+                size="sm"
+                variant="danger"
+                @click="deleteExpense(expense)"
+                class="float-right"
+              >
+                X
+              </b-button>
+            </b-col>
+          </b-form-row>
+
           <b-form-row>
             <b-col>
               <b-form-group
@@ -56,19 +97,6 @@
                   id="expenseConcept"
                   v-model="expense.concept"
                   placeholder="Concept of the expense"
-                />
-              </b-form-group>
-
-              <b-form-group
-                horizontal
-                id="expenseDateGroup"
-                label="Date" label-for="expenseDate"
-              >
-                <b-form-input
-                  required
-                  id="expenseDate"
-                  v-model="expense.date"
-                  type="date"
                 />
               </b-form-group>
             </b-col>
@@ -91,22 +119,62 @@
                   value-field="id"
                 />
               </b-form-group>
+            </b-col>
+          </b-form-row>
 
+          <b-form-row>
+            <b-col>
+              <b-input-group
+                  left="$"
+              >
+                <b-form-input
+                  required
+                  id="expenseAmount"
+                  v-model="expense.amount"
+                  placeholder="0.00"
+                  step="0.01"
+                  min="0"
+                  type="number"
+                />
+              </b-input-group>
+            </b-col>
+
+            <b-col cols="3">
               <b-badge v-if="activity(expense.activity_id).is_billable" variant="success">Billable</b-badge>
               <b-badge v-else variant="secondary">Not Billable</b-badge>
             </b-col>
           </b-form-row>
+
         </b-card>
       </b-card-group>
-      <b-button
-        variant="primary"
-        @click="addExpense">
-        Add Expense
-      </b-button>
 
-      <b-form-row>
-        <b-button type="submit" variant="primary">Submit</b-button>
-      </b-form-row>
+      <hr />
+
+      <b-row align-h="between">
+        <b-col cols="2">
+          <b-button
+            variant="primary"
+            @click="addExpense">
+            + Add Expense
+          </b-button>
+        </b-col>
+        <b-col cols="2">
+          Total: {{total()}}
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col>
+          <b-button
+            type="submit"
+            variant="primary"
+            class="mt-2"
+            block
+          >
+            Submit
+          </b-button>
+        </b-col>
+      </b-row>
     </b-form>
   </b-container>
 </template>
@@ -121,17 +189,24 @@
       ...Vuex.mapGetters(['expenseSubmission', 'activity', 'activities']),
     },
     methods: {
-      getStateVariant(state){
+      total () {
+        return this.expenseSubmission.expenses.map( expense => expense.amount ).reduce( (a, b) => a + parseFloat(b) )
+      },
+      getStateVariant (state) {
         return stateVariant[state]
       },
 
-      addExpense(){
+      addExpense () {
         Promise.resolve(this.expenseSubmission.expenses.push({})).then( () => {
           window.scrollTo(0,document.body.scrollHeight)
         })
       },
 
-      submitExpense(evt) {
+      deleteExpense(expense) {
+        this.expenseSubmission.expenses = this.expenseSubmission.expenses.filter( x => x !== expense)
+      },
+
+      submitExpense (evt) {
         evt.preventDefault()
         alert(JSON.stringify(this.expenseSubmission))
       }
