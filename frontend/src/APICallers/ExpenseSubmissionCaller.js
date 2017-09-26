4988673492ec6fb0f81c904a863e038fd0c17ec8
Nacho García <lupus.zgz@gmail.com>
2017-09-26 11:05:18 +0200

    Change API Cant be modified if it hasn't been reviewed

--- frontend/src/APICallers/ExpenseSubmissionCaller.js
+++ frontend/src/APICallers/ExpenseSubmissionCaller.js
@@ -6,7 +6,7 @@ async function getExpenseSubmissions () {
         date: '2017-04-02',
         concept: 'Qcon London',
         state: 'submitted',
-        modified_by_reviewer: true,
+        modified_by_reviewer: false,
         expenses: [
           { activity_id: 'a1', date: '2017-04-01', concept: 'Train', amount: 9.85 },
           { activity_id: 'a1', date: '2017-04-01', concept: 'Food', amount: 10.85 },
@@ -30,7 +30,7 @@ async function getExpenseSubmissions () {
         date: '2017-08-03',
         concept: 'Barcelona Trip',
         state: 'reviewed',
-        modified_by_reviewer: false,
+        modified_by_reviewer: true,
         expenses: [
           { activity_id: 'a1', date: '2017-04-01', concept: 'Bus', amount: 30.40 },
           { activity_id: 'a1', date: '2017-04-01', concept: 'Food', amount: 10.85 },
