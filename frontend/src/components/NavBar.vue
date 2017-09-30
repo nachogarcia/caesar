93ba0428061336831e700493b374f68e806f800a
Nacho García <lupus.zgz@gmail.com>
2017-09-30 16:09:21 +0200

    Add brand images

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar type="light" variant="info" class="mb-2">
-    <b-navbar-brand tag="h1" href="/">Codurance</b-navbar-brand>
+  <b-navbar variant="light" class="mb-2">
+    <b-navbar-brand href="/"><b-img width="300" src="/static/codurance.png" /></b-navbar-brand>
   </b-navbar>
 </template>
