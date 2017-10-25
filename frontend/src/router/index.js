a51cdd433dfc59683482dd5822dd6f1019e7633e
Nacho García <lupus.zgz@gmail.com>
2017-10-25 16:56:00 +0200

    Stop using async loading for one only component

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-const Expenses = () => import('@/components/Expenses')
+import Expenses from '@/components/Expenses'
 
 Vue.use(Router)
 
@@ -10,6 +10,6 @@ export default new Router({
       path: '/',
       name: 'Expenses',
       component: Expenses
-    },
+    }
   ]
 })
